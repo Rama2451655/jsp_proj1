3c1d1ac83816250be4d61f7df9b327887ed35c67
Rama2451655 <levakuramakrishnareddy4d2@gmail.com>
2023-11-08 16:28:44 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def ramakrishna(request):
+  return HttpResponse("<h1><marquee> hi to my precious girl</marquee></h1>")
+
+# Create your views here.
